44d245952811af2a06c7793c386b74f8e12d680a
MaglooLab <themagloolab@gmail.com>
2025-05-18 14:20:41 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1f7e9a76c31280dfa469cd4852f286bb',
+  rootNotionPageId: '1f7e9a76c312805eb953f65c1a8d36b4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
